09d5f28c57e2e153686f58c9e7f647ee8c14bc67
SomnathKar000 <somnathkar201901@gmail.com>
2023-05-08 23:23:46 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,23 @@ const port = 5000 || process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(
+    express.static(path.resolve(path.dirname(__dirname), "frontend", "build"))
+  );
+  console.log(path.resolve(path.dirname(__dirname), "frontend", "build"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(path.dirname(__dirname), "frontend", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
+}
 
 app.use("/api/v1/user", userRoute);
 
